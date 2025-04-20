e76e8c7090308744585580cd55e2cb156205d9a6
Utilyre <utilyre@gmail.com>
2025-04-20 10:39:03 +0300

    refactor(mything): better stuff

--- cmd/mything/main.go
+++ cmd/mything/main.go
@@ -9,6 +9,7 @@ import (
 	"multiplayer/internal/mcp"
 	"os"
 	"os/signal"
+	"time"
 )
 
 func main() {
@@ -20,77 +21,99 @@ func run() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
 
-	server, err := mcp.Listen(":3000")
+	go startServer(ctx, ":3000")
+	go startClient(ctx, ":3000")
+
+	<-ctx.Done()
+}
+
+func startServer(ctx context.Context, laddr string) {
+	logger := slog.With("type", "server", "laddr", laddr)
+
+	ln, err := mcp.Listen(":3000")
 	if err != nil {
-		slog.Error("failed to start server", "error", err)
+		logger.Error("failed to listen", "error", err)
 		return
 	}
 	defer func() {
-		err = server.Close(ctx)
+		err = ln.Close(ctx)
 		if err != nil {
-			slog.Error("failed to close server", "error", err)
+			logger.Error("failed to close listener", "error", err)
 		}
+		logger.Info("listener closed")
 	}()
-	slog.Info("server started", "address", server.LocalAddr())
 
-	client, err := mcp.Dial(ctx, ":3000")
+	for ctx.Err() == nil {
+		sess, err := ln.Accept(ctx)
+		if err != nil {
+			logger.Warn("failed to accept session", "error", err)
+			continue
+		}
+
+		go func() {
+			logger = logger.With("raddr", sess.RemoteAddr())
+			defer func() {
+				err := sess.Close(context.Background())
+				if err != nil {
+					logger.Error("failed to close session", "error", err)
+					return
+				}
+				logger.Info("session closed")
+			}()
+
+			for ctx.Err() == nil {
+				data, err := sess.Receive(ctx)
+				if errors.Is(err, mcp.ErrClosed) {
+					break
+				}
+				if err != nil {
+					logger.Warn("failed to receive data", "error", err)
+					continue
+				}
+				logger.Info("data received", "data", string(data))
+
+				var idx int
+				_, err = fmt.Sscanf(string(data), "ping %d", &idx)
+				if err != nil {
+					logger.Warn("failed to scan for ping index", "error", err)
+					continue
+				}
+				data = []byte(fmt.Sprintf("pong %d", idx))
+				err = sess.Send(ctx, data)
+				if err != nil {
+					logger.Warn("failed to send back data", "data", string(data), "error", err)
+					continue
+				}
+				logger.Info("sent back data", "data", string(data))
+			}
+		}()
+	}
+}
+
+func startClient(ctx context.Context, raddr string) {
+	logger := slog.With("type", "client", "raddr", raddr)
+
+	sess, err := mcp.Dial(ctx, raddr)
 	if err != nil {
-		slog.Error("failed to start client", "error", err)
+		logger.Error("failed to dial", "error", err)
 		return
 	}
+	logger = logger.With("laddr", sess.LocalAddr())
 	defer func() {
-		err = client.Close(ctx)
+		err := sess.Close(ctx)
 		if err != nil {
-			slog.Error("failed to close client", "error", err)
+			logger.Error("failed to close session", "error", err)
 		}
 	}()
-	slog.Info("client dialed server", "address", client.LocalAddr())
 
-	go provider(ctx, client)
-
-	for ctx.Err() == nil {
-		sess, err := server.Accept(ctx)
-		if errors.Is(err, mcp.ErrClosed) {
-			slog.Info("connection closed")
-			break
-		}
-		if err != nil {
-			slog.Error("failed to accept session", "error", err)
-			continue
-		}
-		slog.Info("session accepted", "address", sess.RemoteAddr())
-
-		go consumer(ctx, sess)
-	}
-}
-
-func consumer(ctx context.Context, sess *mcp.Session) {
-	// when this is uncommented, everything must be closed properly
-	// defer func() {
-	// 	err := sess.Close(ctx)
-	// 	if err != nil {
-	// 		slog.Error("failed to close consumer (server) session", "error", err)
-	// 	}
-	// }()
-	for ctx.Err() == nil {
-		data, err := sess.Receive(ctx)
-		if err != nil {
-			slog.Error("failed to receive data", "error", err)
-			continue
-		}
-
-		slog.Info("received data",
-			"remote", sess.RemoteAddr(), "data", string(data))
-	}
-}
-
-func provider(ctx context.Context, sess *mcp.Session) {
 	for i := range 10 {
-		data := []byte(fmt.Sprintf("ping %d", i))
-		err := sess.Send(ctx, data)
+		data := fmt.Sprintf("ping %d", i)
+		err := sess.Send(ctx, []byte(data))
 		if err != nil {
-			slog.Error("failed to send data", "data", string(data), "error", err)
+			logger.Warn("failed to send data", "data", data, "error", err)
+			continue
 		}
-		slog.Info("sent data", "remote", sess.RemoteAddr(), "data", string(data))
+		logger.Info("sent data", "data", data)
 	}
+	time.Sleep(time.Second) // TODO: make it that the session won't be closed until outbox is empty
 }
