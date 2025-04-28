4197fb8596a2e8593f5275ca7ff74afdb21f0d34
Utilyre <utilyre@gmail.com>
2025-04-28 14:55:02 +0300

    feat(config): use slog json handler when not tty (#11)

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,18 +11,26 @@ import (
 
 func init() {
 	w := os.Stderr
-	logger := slog.New(tint.NewHandler(w, &tint.Options{
-		Level:      slog.LevelDebug,
-		TimeFormat: time.TimeOnly,
-		NoColor:    !isatty.IsTerminal(w.Fd()),
-		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
-			if err, ok := a.Value.Any().(error); ok {
-				aErr := tint.Err(err)
-				aErr.Key = a.Key
-				return aErr
-			}
-			return a
-		},
-	}))
+	var handler slog.Handler
+	if isatty.IsTerminal(w.Fd()) {
+		handler = tint.NewHandler(w, &tint.Options{
+			Level:      slog.LevelDebug,
+			TimeFormat: time.TimeOnly,
+			ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+				if err, ok := a.Value.Any().(error); ok {
+					aErr := tint.Err(err)
+					aErr.Key = a.Key
+					return aErr
+				}
+				return a
+			},
+		})
+	} else {
+		handler = slog.NewJSONHandler(w, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		})
+	}
+
+	logger := slog.New(handler)
 	slog.SetDefault(logger)
 }
