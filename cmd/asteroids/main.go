9c1f2336429acb6aac2d3b1aaf3ea6b75f86094a
Utilyre <utilyre@gmail.com>
2025-05-04 09:00:32 +0300

    include everything in a single binary called asteroids

---
+++ cmd/asteroids/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"embed"
+	"flag"
+	"log/slog"
+	"multiplayer/internal/cli"
+	_ "multiplayer/internal/config"
+	"multiplayer/internal/game"
+	"multiplayer/internal/simulation"
+	"os"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+//go:embed assets
+var assetFS embed.FS
+
+func main() {
+	var (
+		serverAddr string
+		remoteAddr string
+	)
+	flag.StringVar(&serverAddr, "listen", "", "specify address to listen on")
+	flag.StringVar(&remoteAddr, "connect", "", "specify remote address for connecting to a server")
+	flag.Parse()
+
+	ctx, cancel := cli.NewSignalContext()
+	defer cancel()
+
+	if len(serverAddr) > 0 {
+		listenAndSimulate(ctx, serverAddr)
+	} else if len(remoteAddr) > 0 {
+		connectAndRun(ctx, remoteAddr)
+	} else {
+		slog.Error("please specify either a -listen flag or a -connect flag")
+		os.Exit(1)
+	}
+}
+
+func listenAndSimulate(ctx context.Context, addr string) {
+	sim, err := simulation.New(addr)
+	if err != nil {
+		slog.Error("failed to instantiate simulation", "error", err)
+		return
+	}
+	defer func() {
+		err = sim.Close(ctx)
+		if err != nil {
+			slog.Error("failed to close simulation", "error", err)
+		}
+	}()
+
+	ebiten.SetWindowTitle("Asteroids [SERVER]")
+	ebiten.SetTPS(10)
+	err = ebiten.RunGame(sim)
+	if err != nil {
+		slog.Error("failed to run simulation as an ebiten game", "error", err)
+		return
+	}
+}
+
+func connectAndRun(ctx context.Context, raddr string) {
+	g, err := game.New(ctx, raddr)
+	if err != nil {
+		slog.Error("failed to initialize game", "error", err)
+		return
+	}
+	defer func() {
+		err = g.Close(ctx)
+		if err != nil {
+			slog.Error("failed to close game", "error", err)
+		}
+	}()
+
+	ebiten.SetWindowTitle("Asteroids")
+	err = ebiten.RunGame(g)
+	if err != nil {
+		slog.Error("failed to run game as an ebiten game", "error", err)
+		return
+	}
+}
